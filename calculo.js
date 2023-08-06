cb585135098b874b8907ee731a1ce96e9aeda11f
Iasmincost134 <133365214+Iasmincost134@users.noreply.github.com>
2023-08-06 19:55:05 -0300

    Add files via upload

--- calculo.js
+++ calculo.js
@@ -9,15 +9,28 @@ function Chamacilindro (){
 function Chamacone (){
     const pi = 3.14
     let raioCone = Number(prompt("Qual o valor do raio da base do cone?"))
-    let alturaCone = Number(prompt("Qual o valor da altura do cilindro?"))
+    let alturaCone = Number(prompt("Qual o valor da altura do cone?"))
     let valorCone = ((pi*(raioCone*raioCone))*alturaCone/3)
     alert(`o valor do volume do cone é de ${valorCone} m³`)
 }
+ function ChamaEsfera (){
+     const pi = 3.14
+     let raioEsfera = Number(prompt("Qual o valor do raio da Esfera?"))
+     let valorEsfera = (4*pi*(raioEsfera*raioEsfera*raioEsfera))/3
+     alert(`o valor do volume da esfera é de ${valorEsfera} m³`)
+ }
 
-// function Chamaesfera(){
-//     const pi = 3.14
-//     let raioEsfera = Number(prompt("Qual o valor do raio da base do Esfera?"))
-//     let alturaEsfera = Number(prompt("Qual o valor da altura do Esfera?"))
-//     let valorEsfera = ((pi*(raioCone*raioCone))*alturaCone/3)
-//     alert(`o valor do volume do esfera é de ${valorEsfera} m³`)
-// }
+ function ChamaParalelepipedo (){
+    let compriParalelepipedo = Number(prompt("Qual o comprimento do Paralelepípedo?"))
+    let larguraParalelepipedo = Number(prompt("Qual a largura do Paralelepípedo?"))
+    let alturaParalelepipedo = Number(prompt("Qual a altura do Paralelepípedo?"))
+    let valorParalelepipedo = ((compriParalelepipedo*larguraParalelepipedo)*alturaParalelepipedo)
+    alert(`o volume do paralelepípedo é de ${valorParalelepipedo} m³`)
+}
+
+function ChamaPiramede (){
+    let areaPiramede = Number(prompt("Qual o valor da área da base da pirâmede?"))
+    let alturaPiramide = Number(prompt("Qual a altura da pirâmede?"))
+    let valorPiramede = ((areaPiramede*areaPiramede)*alturaPiramide)/3
+    alert(`o volume da pirâmede é de ${valorPiramede} m³`)
+}
